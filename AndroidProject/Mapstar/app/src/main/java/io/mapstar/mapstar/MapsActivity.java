4f7a39af261ad37d5ae2264b2937edf39b4b04f2
Lennart Wachowiak <lennartw@mail.uni-paderborn.de>
2016-06-11 17:12:03 +0200

    options activity added

--- AndroidProject/Mapstar/app/src/main/java/io/mapstar/mapstar/MapsActivity.java
+++ AndroidProject/Mapstar/app/src/main/java/io/mapstar/mapstar/MapsActivity.java
@@ -1,7 +1,10 @@
 package io.mapstar.mapstar;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button calcBtn = (Button) findViewById(R.id.test_Btn);
+        calcBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(getApplicationContext(), Options_Activity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 
