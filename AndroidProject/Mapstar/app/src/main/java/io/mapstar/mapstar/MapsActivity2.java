229d0722d7cd84fea8855ae71ab940c1f348c6f0
Felix Schmoll <eggi.innovations@gmail.com>
2016-06-12 07:43:14 +0200

    Fix PolyLine

--- AndroidProject/Mapstar/app/src/main/java/io/mapstar/mapstar/MapsActivity2.java
+++ AndroidProject/Mapstar/app/src/main/java/io/mapstar/mapstar/MapsActivity2.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +34,24 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient
+                .Builder(this)
+                .addApi(AppIndex.API)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+
+        client.connect();
+
         Intent inputIntent = getIntent();
         ArrayList<Business> businesses = (ArrayList<Business>) inputIntent.getSerializableExtra("sights");
         double longitude = inputIntent.getDoubleExtra("longitude", 0);
         double latitude = inputIntent.getDoubleExtra("latitude", 0);
 
 
-        Polyline line = mMap.addPolyline(new PolylineOptions()
-                .add(new LatLng(48.9935, 8.4022), new LatLng(latitude, longitude))
-                .width(5)
-                .color(Color.RED));
+
 
     }
 
@@ -61,5 +73,10 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(48.9935, 8.4022), new LatLng(30, 12))
+                .width(5)
+                .color(Color.RED));
     }
 }
